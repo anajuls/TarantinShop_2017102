be7ef3dc144329b927d400185cc2d9070b7b2ac7
anajuls <wedesignstudio.dublin@gmail.com>
2020-04-26 11:24:43 +0000

    1commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Gitpod Ready-to-Code](https://img.shields.io/badge/Gitpod-Ready--to--Code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/anajuls/TarantinShop_2017102) 
+
 # TarantinShop_2017102
---
+++ app.js
@@ -0,0 +1,13 @@
+const htpp = require('express');
+
+const app = express();
+
+app.use((req, res,next)) => {
+    
+    res.status(200).json({
+
+        message: 'It works!'
+    });
+});
+
+module.exports = app;
